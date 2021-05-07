0cb2b7de5eb9369a9d96e6e474e833d1ced30aab
kenamehta <kenamehta1995@gmail.com>
2021-05-07 12:28:26 -0700

    insider threats

--- src/script.js
+++ src/script.js
@@ -4,8 +4,10 @@ var numWrong = 0;
 var numRight = 0;
 var phraseLength = 0;
 var numChar = 0;
-var securityQuestions = ["A malware that threatens to publish or blocks access to data, usually by encrypting it, until the victim pays a ransom fee to the attacker", "An attack that floods a computer or network so it can’t respond to requests", "An attack that occurs when hackers insert themselves into a two-party transaction", "A computer program that, when executed, replicates itself by modifying other computer programs and inserting its own code", "A software that enables a user to obtain covert information about another's computer activities by transmitting data covertly from their hard drive"];
-var securityAnswers = ["Ransomeware", "Denial of Service", "Man in the Middle", "Virus", "Spyware"];
+var threatQuestions = ["A malware that threatens to publish or blocks access to data, usually by encrypting it, until the victim pays a ransom fee to the attacker", "An attack that floods a computer or network so it can’t respond to requests", "An attack that occurs when hackers insert themselves into a two-party transaction", "A computer program that, when executed, replicates itself by modifying other computer programs and inserting its own code", "A software that enables a user to obtain covert information about another's computer activities by transmitting data covertly from their hard drive"];
+var threatAnswers = ["Ransomeware", "Denial of Service", "Man in the Middle", "Virus", "Spyware"];
+var insiderQuestions = ["Some who has legitimate access to the system but is abusing their privileges", "An entity of trust who misuses their privileges, deliberately or accidentally, in a way that constitutes a threat", "An insider’s use of IT to direct specific harm at an organization or an individual", "An insider’s use of IT to steal IP from the organization. This category includes industrial espionage involving outsiders"];
+var insiderAnswers = ["Insider threats", "Accidental threats", "IT sabotage", "Theft of intellectual property"];
 // var phrases = ["Early bird gets the worm", "Read between the lines", "I can eat a horse", "Twenty-four seven", "Cat got your tounge", "One in one million", "I beg to differ", "Easier said than done", "Add insult to injury", "Don't cry over spilled milk", "Curiosity killed the cat", "Two peas in a pod", "That's the last straw", "Piece of cake", "Speak of the devil", "Go the whole nine yards", "An eye for an eye", "Hit the hay", "Stab someone in the back", "Quit cold turkey", "Cut to the chase", "Best of both worlds", "Kill two birds with one stone", "Break a leg", "Hit the nail on the head", "Kick the bucket"];
 var movies = ["Frankly, my dear, I don't give a damn", "May the Force be with you", "Titanic", "After all this time? Always.", "Life is like a box of chocolates", "To infinity and beyond", "I'll be back", "Wax on, wax off", "I'm going to make him an offer he can't refuse", "Want to know how I got these scars?", "Citizen Kane", "Team Edward", "No, I am your father", "Do or do not. There is no try.", "Do you want to build a snowman?", "Hakuna Matata", "Go ahead, make my day", "Bond. James Bond.", "You're going to need a bigger boat", "You talkin' to me?", "Here's Johnny!", "The hills are alive!", "Toto, I have a feeling we're not in Kansas anymore", "Welcome to Jurassic Park", "Say hello to my little friend!", "Run Forest Run!"];
 var songs = ["I came in like a wrecking ball", "Yesterday all my troubles seemed so far away", "Work, work, work, work, work", "Just a small town girl", "I still haven't found what I'm looking for", "Annie are you OK?", "Nah nah nah nah nah nah nah, nah nah nah nah, hey Jude", "Is this the real life? Is this just fantasy?", "We don't need no education", "Have you ever seen the rain?", "She's got eyes of the bluest skys", "So close, no matter how far", "I don't want to miss a thing", "Take my hand, we'll make it, I swear", "That's me in the corner", "A mulatto, an albino, a mosquito, my libido", "You've been thunderstruck", "Another one bites the dust", "Eye of the tiger", "B-B-B-Bennie and the Jets", "Sing us a song you're the piano man", "Hello darkness my old friend", "Somewhere over the rainbow blue birds fly", "Don't worry about a thing", "Imagine there's no heaven", "Can you feel the love tonight?"];
@@ -24,19 +26,20 @@ function mp() {
 }
 
 function threats() {
-    rand = Math.floor(Math.random() * securityAnswers.length);
-    word = securityAnswers[rand];
+    rand = Math.floor(Math.random() * threatAnswers.length);
+    word = threatAnswers[rand];
     document.getElementById('singlePage').style.display = "none";
-    document.getElementById('categoryName').innerHTML = "Threats";
-    document.getElementById('question').innerHTML = securityQuestions[rand];
+    document.getElementById('categoryName').innerHTML = "Cyber Threats";
+    document.getElementById('question').innerHTML = threatQuestions[rand];
     hangman();
 }
 
-function security() {
-    rand = Math.floor(Math.random() * movies.length);
-    word = movies[rand];
+function insider() {
+    rand = Math.floor(Math.random() * insiderAnswers.length);
+    word = insiderAnswers[rand];
     document.getElementById('singlePage').style.display = "none";
-    document.getElementById('categoryName').innerHTML = "Movies and movie quotes";
+    document.getElementById('categoryName').innerHTML = "Insider Threats";
+    document.getElementById('question').innerHTML = insiderQuestions[rand];
     hangman();
 }
 
